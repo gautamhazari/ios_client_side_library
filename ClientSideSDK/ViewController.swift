0ca4d81917fac87c6deffe85c297e30c1d483b40
dMaretsky <user@gsma0.itransition.corp>
2018-01-20 12:18:16 +0300

    Initial Commit

---
+++ ClientSideSDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClientSideSDK
+//
+//  Created by user on 1/20/18.
+//  Copyright © 2018 a1qa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
