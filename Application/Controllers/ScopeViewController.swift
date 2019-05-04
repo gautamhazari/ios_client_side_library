99f9186cb30372469f32691b6450a193250c3a61
PolinaTitovaA1QA <p.titova@a1qa.com>
2019-05-04 17:23:40 +0300

    Add scopes

---
+++ Application/Controllers/ScopeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScopeViewController.swift
+//  ClientSideLib
+//
+//  Created by user on 5/3/19.
+//  Copyright © 2019 a1qa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ScopeViewContrioller: BaseViewController {
+    // The sample values
+    
+   
+}
