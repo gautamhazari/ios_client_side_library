bbedce901abb4f574730192dbb4f0c6af703d2d3
PolinaTitovaA1QA <p.titova@a1qa.com>
2019-05-10 14:51:48 +0300

    add controller for scopes

--- Application/Controllers/ScopeViewController.swift
+++ Application/Controllers/ScopeViewController.swift
@@ -11,5 +11,52 @@ import UIKit
 class ScopeViewContrioller: BaseViewController {
     // The sample values
     
-   
+    @IBOutlet weak var openid: UISwitch!
+    @IBOutlet weak var mcAttrVmShare: UISwitch!
+    @IBOutlet weak var mcAttrVmShareHash: UISwitch!
+    @IBOutlet weak var scopeLabel: UILabel!
+    @IBOutlet weak var mcMnvValidate: UISwitch!
+    @IBOutlet weak var mcMnvValidatePlus: UISwitch!
+    @IBOutlet weak var mcIndiaTc: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        setSwitches(switchOn: openid, switchArray: getSwitches());
+    }
+    
+    func getSwitches() -> [UISwitch] {
+        return [openid, mcAttrVmShareHash, mcAttrVmShare, mcMnvValidate, mcMnvValidatePlus, mcIndiaTc];
+    }
+    
+    func setSwitches(switchOn: UISwitch, switchArray: [UISwitch]){
+        for switchElement in switchArray {
+            switchElement.isOn = false;
+        }
+        switchOn.isOn = true;
+    }
+    
+    @IBAction func openid(_ sender: Any, forEvent event: UIEvent) {
+        setSwitches(switchOn: openid, switchArray: getSwitches());
+    }
+    
+    @IBAction func mcAttrVmShare(_ sender: Any, forEvent event: UIEvent) {
+        setSwitches(switchOn: mcAttrVmShare, switchArray: getSwitches());
+    }
+    
+    @IBAction func mcAttrVmShareHash(_ sender: Any, forEvent event: UIEvent) {
+        setSwitches(switchOn: mcAttrVmShareHash, switchArray: getSwitches())
+    }
+    
+    @IBAction func mcMnvValidate(_ sender: Any, forEvent event: UIEvent) {
+        setSwitches(switchOn: mcMnvValidate, switchArray: getSwitches())
+    }
+    
+    @IBAction func mcMnvValidatePlus(_ sender: Any, forEvent event: UIEvent) {
+        setSwitches(switchOn: mcMnvValidatePlus, switchArray: getSwitches())
+    }
+    
+    @IBAction func mcIndiaTc(_ sender: Any, forEvent event: UIEvent) {
+        setSwitches(switchOn: mcIndiaTc, switchArray: getSwitches())
+    }
+    
 }
